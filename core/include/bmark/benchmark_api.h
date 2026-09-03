610f988187d8dfaac4efe2a4ea1b1876cef7578e
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-09-03 17:05:25 +0200

    feat(api): expose benchmark functions

--- core/include/bmark/benchmark_api.h
+++ core/include/bmark/benchmark_api.h
@@ -1,4 +1,10 @@
 #ifndef BMARK_BENCHMARK_API_H
 #define BMARK_BENCHMARK_API_H
 
+#include "types.h"
+
+t_bench	*create_bench(char *name);
+t_rv	bench_run(t_bench *bench, void (*f)(void));
+void	bench_print(t_bench *bench);
+
 #endif //BMARK_BMARK_API_H
